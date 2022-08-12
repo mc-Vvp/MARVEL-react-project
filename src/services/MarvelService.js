60e0ba43c32c6080c1e6fa8f9e6b39295ac45d08
mc-Vvp <maximvaisman2801@gmail.com>
2022-08-12 16:18:17 +0300

    added id to characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,7 +13,7 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=230&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=1200&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -24,6 +24,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbanil: char.thumbnail.path + "." + char.thumbnail.extension,
