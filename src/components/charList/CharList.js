53eba0bf78ff7ef184dd784355d7b333b3e9213e
mc-Vvp <maximvaisman2801@gmail.com>
2022-08-13 21:12:31 +0300

    added type checking

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from "prop-types"
+
 import MarvelService from '../../services/MarvelService';
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -102,6 +104,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
